afdebb04ce684529adb3085a72de657fbd102b76
alexpromo <alexsandro@promofarma.com.br>
2026-06-10 13:32:52 -0300

    Create zabbix_inventory.py

---
+++ zabbix_inventory.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests
+import json
+
+ZABBIX_URL = "http://192.168.1.14/api_jsonrpc.php"
+ZABBIX_TOKEN = "cfbf448e275112250c7fe06ef9e84505fe5ea46b880cc3e46c7671267d5cb432"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {ZABBIX_TOKEN}"
+}
+
+# BUSCAR HOSTS
+payload = {
+    "jsonrpc": "2.0",
+    "method": "host.get",
+    "params": {
+        "output": ["host"],
+        "selectInterfaces": ["ip"]
+    },
+    "id": 1
+}
+
+response = requests.post(ZABBIX_URL, headers=headers, json=payload).json()
+
+print("DEBUG:", response)
+
+hosts = response.get("result", [])
+
+inventory = {
+    "all": {
+        "hosts": []
+    }
+}
+
+for h in hosts:
+    for i in h["interfaces"]:
+        if i["ip"]:
+            inventory["all"]["hosts"].append(i["ip"])
+
+print(json.dumps(inventory))
