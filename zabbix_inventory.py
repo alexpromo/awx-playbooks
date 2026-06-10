056dfeea64d97587520dd2ac5523e79719d32664
alexpromo <alexsandro@promofarma.com.br>
2026-06-10 13:51:30 -0300

    Update zabbix_inventory.py

--- zabbix_inventory.py
+++ zabbix_inventory.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python3
 import requests
 import json
 
 ZABBIX_URL = "http://192.168.1.14/api_jsonrpc.php"
 ZABBIX_TOKEN = "cfbf448e275112250c7fe06ef9e84505fe5ea46b880cc3e46c7671267d5cb432"
+GROUP_ID = "30"  # <-- seu grupo
 
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {ZABBIX_TOKEN}"
 }
 
-# BUSCAR HOSTS
+
 payload = {
     "jsonrpc": "2.0",
     "method": "host.get",
     "params": {
         "output": ["host"],
-        "selectInterfaces": ["ip"]
+        "selectInterfaces": ["ip"],
+        "groupids": [GROUP_ID]   # <-- AQUI É O SEGREDO
     },
     "id": 1
 }
@@ -34,8 +35,8 @@ inventory = {
 }
 
 for h in hosts:
-    for i in h["interfaces"]:
-        if i["ip"]:
+    for i in h.get("interfaces", []):
+        if i.get("ip"):
             inventory["all"]["hosts"].append(i["ip"])
 
-print(json.dumps(inventory))
+print(json.dumps(inventory, indent=2))
